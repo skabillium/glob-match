4b00ad4f94d1c188f492f6486f3991e442d91c65
Bill Kyriazopoulos <skabillium@gmail.com>
2024-02-03 16:11:32 +0200

    Implement '?' & '*' support
    Not a solution I am proud of but it will do the trick for now

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,60 @@
-export function check(pattern: string, ...strs: string[]) {
-    console.log('TODO: This will check if a string or list of strings match a glob pattern');
-}
-
 export function toRegex(pattern: string) {
-    console.log("TODO: This will return a new Regex based on a glob pattern provided");
+    return new RegExp(pattern);
 }
 
+type CheckOptions = {
+    pStart: number;
+    sStart: number;
+};
+
+export function check(
+    pattern: string,
+    str: string,
+    opts?: CheckOptions,
+): boolean {
+    let p = opts?.pStart ?? 0;
+    let s = opts?.sStart ?? 0;
+
+    while (p !== pattern.length && s !== str.length) {
+        switch (pattern[p]) {
+            case '?':
+                p++;
+                s++;
+                break;
+
+            case '*':
+                const result = check(pattern, str, {
+                    pStart: p + 1,
+                    sStart: s,
+                });
+                if (result) {
+                    return result;
+                }
+                s++;
+                break;
+            case '[':
+                throw new Error('TODO: Char list not implemented');
+            case '\\':
+                throw new Error('TODO: Escape not implemented');
+            default:
+                if (pattern[p] === str[s]) {
+                    p++;
+                    s++;
+                } else {
+                    return false;
+                }
+        }
+    }
+
+    if (s === str.length) {
+        while (pattern[p] === '*') {
+            p++;
+        }
+
+        if (p === pattern.length) {
+            return true;
+        }
+    }
+
+    return false;
+}
